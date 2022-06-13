54e7abc74f91c6621d4e880eb73b7a6885481782
esfir <zvezda1997@gmail.com>
2022-06-13 23:06:44 +0300

    saction 7 commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import '../styles/globals.css';
 import Head from "next/head";
 import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <>
+  return (
+    <>
       <Head>
         <title>MyTop - наш лучший топ</title>
         <link key={1} rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"/>
       </Head>
       <Component {...pageProps} />
-    </>;
+    </>
+  );
 }
 
 export default MyApp;
