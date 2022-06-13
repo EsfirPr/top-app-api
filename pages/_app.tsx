1eddc3dd1061e46745e9d1e3ea57d137f2b94a5a
esfir <zvezda1997@gmail.com>
2022-06-13 22:17:45 +0300

    saction 5 commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import '../styles/globals.css';
+import Head from "next/head";
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link key={1} rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>;
 }
 
 export default MyApp;
